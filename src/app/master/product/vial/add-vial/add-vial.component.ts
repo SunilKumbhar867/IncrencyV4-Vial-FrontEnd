48058769e986796de3ba712f7f3d371350eaa059
SunilKumbhar867 <38717402+SunilKumbhar867@users.noreply.github.com>
2019-07-22 17:31:00 +0530

    new form for vial add product

--- src/app/master/product/vial/add-vial/add-vial.component.ts
+++ src/app/master/product/vial/add-vial/add-vial.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { DataService } from '../../../../services/commonData/data.service';
-import { JsonDataService } from '../../../../services/commonData/json-data.service';
-import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { ValidationService } from '../../../../services/validations/validation.service';
 import { MatDialog } from '@angular/material';
 import { RemarkComponent } from '../../../../shared/remark/remark/remark.component';
 import { SessionStorageService } from 'ngx-webstorage';
 import { HttpService } from '../../../../services/http/http.service';
-import { NgbPanelChangeEvent, NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
-import { ErrorHandlingService } from "../../../../services/error-handling/error-handling.service";
-import { UserService } from '../../../../services/user/user.service';
+import { Router } from '@angular/router';
 
+
+declare var swal: any;
 @Component({
   selector: 'app-add-vial',
   templateUrl: './add-vial.component.html',
@@ -23,125 +21,152 @@ export class AddVialComponent implements OnInit {
   str_LblBFGCode  : string = '';
   bln_Loading     : boolean;
   bln_isRemarkPopupOpened: boolean;
-  constructor(private fb: FormBuilder,private http ?: HttpService,
-    private validation?: ValidationService,
-    private sessionStorage?: SessionStorageService,
-    private errorHandling?: ErrorHandlingService,private dialog?: MatDialog,
-    private userService?: UserService) { }
+  arrPrdCombination : any;
+
+  constructor(
+              private fb : FormBuilder ,
+              private sessionStorage?: SessionStorageService , 
+              private http ?: HttpService , 
+              private validation?: ValidationService,
+              private dialog?: MatDialog,
+              public router?: Router
+              ){}
 
   ngOnInit() {
-    this.addVialForm = new FormGroup({
-      'str_ProductCode' : new FormControl(null, Validators.required),
-      'str_ProductName' : new FormControl(null,Validators.required),
-      'str_Version' : new FormControl(null, Validators.required)
-    });
-  }
-  /*onSubmit(){
-    //alert(this.addVialForm.value.str_ProductCode);
-    swal({
-      title: 'Are you sure ?',
-      text: "Do you want to add this product ?",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'No'
-    }).then((result) =>
-    {
-      if (result)
-      {
-        this.bln_Loading = false;
-        this.bln_isRemarkPopupOpened = true;
-        const message = { message : 'Stage'  };
-        const dialogRef = this.dialog.open(RemarkComponent, {
-          data: message,
-          width: '570px',
-          disableClose: true
-        });
+    this.addVialForm = this.fb.group({
+      str_ProductCode : ['',Validators.compose([
+            this.validation.requiredField
+      ])],
+      str_ProductName : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_Version : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_ManufCode : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_StdWt : ['',Validators.compose([
+        this.validation.requiredField
+      ])]
+    })
 
-        dialogRef.afterClosed().subscribe(result => {
-          if (result !== undefined) //remark data
-          {
-            const remark = result.reason;
-            const obj_sendData = this.getDataToSendOnSubmit(remark);
-            this.bln_Loading = true;
-            this.http.postMethod('stage/storeStages', obj_sendData).subscribe(res=>{
-            this.bln_Loading = false;
-            this.objarr_apiSubmitData = res;
-
-            if (this.objarr_apiSubmitData.result === 'Stage already exist')
-            {
-              this.bln_Loading = false;
-              swal({
-                title: "Stage already exist!",
-                text: "",
-                type: "warning",
-                allowOutsideClick: false,
-                },);
-              this.frm_stage.value.str_stage = '';
-            }
-            else if (this.objarr_apiSubmitData.result === 'Stage Added Successfully') {
-              swal({
-                title: "Stage Added Successfully!",
-                text: "",
-                type: "success",
-                allowOutsideClick: false,
-                },);
-
-              this.onReset();
-              this.getStageData();
-              this.dataTableReset();
-            }
-            else
-            {
-              swal({
-                title: "Can not Add Stage, Try again!",
-                text: "",
-                type: "error",
-                allowOutsideClick: false,
-                },);
-            }
-            },
-            err => {
-              this.errorHandling.checkError(err.status);
-              this.bln_Loading = false;
-            });
-          }
-        });
+    this.http.getMethod(`product/get`).subscribe(
+      (res : any ) => {
+        this.arrPrdCombination = res;
       }
-    }, function (dismiss) { });
-  }*/
-  getDataToSendOnSubmit(str_remark:any)
-  {
-    const str_ProductCode = this.addVialForm.value.str_ProductCode;
-    const str_ProductName = this.addVialForm.value.str_ProductName;
-    const str_Version = this.addVialForm.value.str_Version;
-
-     /**Audit Trail Data */
-     const str_action = "Add";
-     const str_userID = this.sessionStorage.retrieve("userId");
-     const str_userName = this.sessionStorage.retrieve("userName");
-     /**End Audit Trail Data */
-
-      /**Activity Log */
-      const str_activity = "Vial Added";
-      /**End Activity Log */
-
-     this.obj_submitData =
-     {
-       "str_ProductCode": str_ProductCode,
-       "str_ProductName": str_ProductName,
-       "str_Version": str_Version,
-       "Action": str_action,
-       "Remark": str_remark,
-       "username": str_userName,
-       "userid": str_userID,
-       "activity": str_activity
-     };
-     const data:Object={};
-     Object.assign(data,this.obj_submitData);
-     return data;
+    )
   }
+
+  addVial()
+  {
+    var Obj_AddVial = this.addVialForm;
+    // console.log(Obj_AddVial);
+    
+    var strPrdId = this.addVialForm.get("str_ProductCode").value;
+    var strPrdName = this.addVialForm.get("str_ProductName").value;
+    var strPrdVersion = this.addVialForm.get("str_Version").value;
+    var strMfgCode = this.addVialForm.get("str_ManufCode").value;
+    var intStdWt = this.addVialForm.get("str_StdWt").value;
+    var intStdDp = this.validation.getDPValue(intStdWt);
+
+    var arrObjProduct = this.arrPrdCombination.find(k=>k.ProductId == strPrdId);
+    /**
+     * Construct Object Of Product Detail
+     */
+    const ObjectData: object = {};
+    
+
+    if(arrObjProduct == undefined)
+    {
+        swal({
+          title: "Are you sure ?",
+          text:"Do you want to add ",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes",
+          cancelButtonText: "No"
+        }).then(
+          result =>{
+            if(result)
+            {
+              this.bln_Loading = false;
+              this.bln_isRemarkPopupOpened = true;
+              const message = { message: "Add Vial" };
+              const dialogRef = this.dialog.open(RemarkComponent, {
+                data: message,
+                width: "570px",
+                disableClose: true
+              });
+
+              dialogRef.afterClosed().subscribe(result =>{
+                if(result !== undefined)
+                {
+                  const remark = result.reason;
+                  const str_activity = "Vial Added";
+                  const str_userID = this.sessionStorage.retrieve("userId");
+                  const str_userName = this.sessionStorage.retrieve("userName");
+                  
+                  Object.assign(
+                    ObjectData,
+                    { ProductId: strPrdId },
+                    { ProductName: strPrdName },
+                    { Version: strPrdVersion},
+                    { MfgCode: strMfgCode},
+                    { StdWt : intStdWt},
+                    { StdDp : intStdDp},
+                    { UserID : str_userID},
+                    { UserName : str_userName},
+                    { Activity : str_activity},
+                    { Remark : remark}
+                  );
+                  console.log(JSON.stringify(ObjectData));
+
+                  //   this.http.postMethod(`product/addVial` , ObjectData).subscribe(
+                  //   (res : any) =>{
+                  //     console.log(res);
+                  //   }
+                  // );
+
+                }
+              })
+            }
+
+          },
+          function(dismiss){}
+        );
+
+        // console.log(ObjectData);
+    }
+    else{
+         
+      if(
+         (arrObjProduct.ProductName.toUpperCase() == strPrdName.toUpperCase())  && 
+         (arrObjProduct.Version.toUpperCase() == strPrdVersion.toUpperCase()) &&
+         (arrObjProduct.ProductVersion.toUpperCase() == strMfgCode.toUpperCase()) 
+         )
+      {
+        swal({
+          title: "Product Combination Is Already Exists",
+          text: "",
+          type: "error",
+          allowOutsideClick: false,
+          },);
+      }
+      else{
+        
+      }
+
+    }
+
+  }
+
+  ViewEditUser()
+  {
+    this.router.navigate(["/master/product/vial/edit-vial"]);
+  }
+  
 
 }
--- src/app/master/product/vial/edit-vial/edit-vial.component.ts
+++ src/app/master/product/vial/edit-vial/edit-vial.component.ts
@@ -1,15 +1,169 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SessionStorageService } from 'ngx-webstorage';
+import { HttpService } from '../../../../services/http/http.service';
+import { ValidationService } from '../../../../services/validations/validation.service';
+import { MatDialog } from '@angular/material';
+import { RemarkComponent } from '../../../../shared/remark/remark/remark.component';
+declare var swal: any;
 @Component({
   selector: 'app-edit-vial',
   templateUrl: './edit-vial.component.html',
   styleUrls: ['./edit-vial.component.css']
 })
+
 export class EditVialComponent implements OnInit {
 
-  constructor() { }
+  editVialForm     : FormGroup;
+  obj_submitData  : any;
+  str_LblBFGCode  : string = '';
+  bln_Loading     : boolean;
+  bln_isRemarkPopupOpened: boolean;
+  arrPrdCombination : any;
+
+  constructor(
+              private fb : FormBuilder ,
+              private sessionStorage?: SessionStorageService , 
+              private http ?: HttpService , 
+              private validation?: ValidationService,
+              private dialog?: MatDialog,
+              public router?: Router
+              ){}
 
   ngOnInit() {
+    this.editVialForm = this.fb.group({
+      str_ProductCode : ['',Validators.compose([
+            this.validation.requiredField
+      ])],
+      str_ProductName : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_Version : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_ManufCode : ['',Validators.compose([
+        this.validation.requiredField
+      ])],
+      str_StdWt : ['',Validators.compose([
+        this.validation.requiredField
+      ])]
+    })
+
+    this.http.getMethod(`product/get`).subscribe(
+      (res : any ) => {
+        this.arrPrdCombination = res;
+      }
+    )
   }
+
+  editVial()
+  {
+    var Obj_AddVial = this.editVialForm;
+    // console.log(Obj_AddVial);
+    
+    var strPrdId = this.editVialForm.get("str_ProductCode").value;
+    var strPrdName = this.editVialForm.get("str_ProductName").value;
+    var strPrdVersion = this.editVialForm.get("str_Version").value;
+    var strMfgCode = this.editVialForm.get("str_ManufCode").value;
+    var intStdWt = this.editVialForm.get("str_StdWt").value;
+    var intStdDp = this.validation.getDPValue(intStdWt);
+
+    var arrObjProduct = this.arrPrdCombination.find(k=>k.ProductId == strPrdId);
+    /**
+     * Construct Object Of Product Detail
+     */
+    const ObjectData: object = {};
+    
+
+    if(arrObjProduct == undefined)
+    {
+        swal({
+          title: "Are you sure ?",
+          text:"Do you want to add ",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes",
+          cancelButtonText: "No"
+        }).then(
+          result =>{
+            if(result)
+            {
+              this.bln_Loading = false;
+              this.bln_isRemarkPopupOpened = true;
+              const message = { message: "Add Vial" };
+              const dialogRef = this.dialog.open(RemarkComponent, {
+                data: message,
+                width: "570px",
+                disableClose: true
+              });
+
+              dialogRef.afterClosed().subscribe(result =>{
+                if(result !== undefined)
+                {
+                  const remark = result.reason;
+                  const str_activity = "Vial Added";
+                  const str_userID = this.sessionStorage.retrieve("userId");
+                  const str_userName = this.sessionStorage.retrieve("userName");
+                  
+                  Object.assign(
+                    ObjectData,
+                    { ProductId: strPrdId },
+                    { ProductName: strPrdName },
+                    { Version: strPrdVersion},
+                    { MfgCode: strMfgCode},
+                    { StdWt : intStdWt},
+                    { StdDp : intStdDp},
+                    { UserID : str_userID},
+                    { UserName : str_userName},
+                    { Activity : str_activity},
+                    { Remark : remark}
+                  );
+                  console.log(JSON.stringify(ObjectData));
+
+                  //   this.http.postMethod(`product/addVial` , ObjectData).subscribe(
+                  //   (res : any) =>{
+                  //     console.log(res);
+                  //   }
+                  // );
+
+                }
+              })
+            }
+
+          },
+          function(dismiss){}
+        );
+
+        // console.log(ObjectData);
+    }
+    else{
+         
+      if(
+         (arrObjProduct.ProductName.toUpperCase() == strPrdName.toUpperCase())  && 
+         (arrObjProduct.Version.toUpperCase() == strPrdVersion.toUpperCase()) &&
+         (arrObjProduct.ProductVersion.toUpperCase() == strMfgCode.toUpperCase()) 
+         )
+      {
+        swal({
+          title: "Product Combination Is Already Exists",
+          text: "",
+          type: "error",
+          allowOutsideClick: false,
+          },);
+      }
+      else{
+        
+      }
+
+    }
+
+  }
+
+
+  
+
 
 }
